0129d46a05f39797039ca1bcba8ded5da73bdbfc
Umair <mrumairhassan@outlook.com>
2022-09-26 20:17:25 +0500

    Routes adds

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 
 function App() {
-  return <Router>
+  return( 
+<Router>
+
+
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="launch">Launch</Link>
+    <Link to="/">Home</Link>
+    <Link to="/">Home</Link>
+    <Link to="/">Home</Link>        
+  </nav>
+  
+  
+
     <Routes>
       <Route path="/"element={<Home />}/>
+      <Route path="launch"element={<Launch />}/>
+      <Route path="/"element={<LaunchIndex/>}/>
+      
     </Routes>
-    
     </Router>
     
-  
+  ); 
 }
 
+function Launch(){
+  return(
+  
+  <div>
+    <h1>Welcome Launch</h1>
+  </div>
+  )
+  
+  }
+
+
+function LaunchIndex(){
+  return(
+    <ul>
+      {Object.entries(shoes).map(([slug,{ name,img}])=>(
+        <li key={slug}>
+          <h2>{name}</h2>
+          <img src={img} alt={name}/>
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 function Home(){
 return(
@@ -25,7 +63,7 @@ return(
 }
 
 export default App;
-
+ 
 
 
 const shoes = {
