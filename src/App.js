6b9822199e046a3ed0d80dc1a35309d981d93f20
Umair <mrumairhassan@outlook.com>
2023-07-27 21:21:20 +0500

    Complete project and fix all bugs

--- src/App.js
+++ src/App.js
@@ -1,103 +1,196 @@
-import React from "react";
-import {BrowserRouter as Router, 
+import {
+  BrowserRouter as Router,
   Routes,
   Route,
-   Link,
-   Outlet} from "react-router-dom";
+  Link,
+  Outlet,
+useParams,
+} from "react-router-dom";
 
 
-function App() {
-  return( 
-<Router>
+function App () {
 
-
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/launch">Launch</Link>
-    <Link to="/launches">LaunchIndex</Link>
-           
-  </nav>
-  
-  
-
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="launch" element={<Launch />}/>
-      <Route path="/launches" element={<LaunchIndex/>}/>
-      <Route path=":slug" element={<LaunchShoe/>}/>
-      <Route/>
-      </Routes>
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/launch">Launch</Link>
+      </nav>
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="launch" element={<Launch/>}>
+      {/* this is nested by this <Route path="launch" element={<Launch/>}></Route> */}
+      <Route path="/launch" element={<LaunchIndex/>}/>
+      <Route path=":slug" element={<LaunchShoe />} />
+      </Route>
+      <Route path="*" element={<NotFound/>}/>
+     </Routes>
     </Router>
-    
-  ); 
+  )
 }
 
+function Home() {
+    return (
+      <div>
+        <h1>Welcome Home</h1>
+      </div>
+    );
+  }
 
 
+   function NotFound() {
+    return (
+      <div>
+        <h1>Sorry your page is not found</h1>
+      </div>
+    );
+  }
 
-
-function Home(){
-return(
-
-<div>
-  <h1>Welcome Home</h1>
-</div>
-);
-
-}
-
-
-function Launch(){
-  return(
+    function Launch() {
+    return (
+      <div>
+        <h1>Launch</h1>
+        
+        <Outlet />
+      </div>
+    );
+  }
   
-  <div>
-    <h1>Launch</h1>
-    <Outlet />
-  </div>
-  );
+  function LaunchIndex() {
+    return (
+      <ul>
+        {Object.entries(shoes).map(([slug, { name, img }]) => (
+          <li key={slug}>
+            <Link to={`/launch/${slug}`}>
+              <h2>{name}</h2>
+              <img src={img} alt={name} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    );
+  }
   
+  function LaunchShoe (){
+    const {slug} = useParams();
+    const shoe = shoes[slug];
+
+    if (!shoe) {
+      return <h2>
+        Not Found
+      </h2>
+    }
+
+    const {name , img} = shoe;
+    return (
+      <div>
+        <h1>{name}</h1>
+        <img src={img} alt={name} />
+      </div>
+    )
   }
 
    
-function LaunchIndex() {
-  return(
-    <ul>
-      {Object.entries(shoes).map(([slug,{ name, img }]) => (
-        <li key={slug}>
-          <Link to={'/Launch/${slug}'}>
-          <h2>{name}</h2>
-          <img src={img} alt={name} />
-          </Link>
-        </li>  
-      ))}
-    </ul>
-  );
-}
-
-function LaunchShoe(){
-
-  return <div>shoe</div>;
-}
- 
- 
-
 
 const shoes = {
-  "air-jordan-3-valor-blue": {
-    name: "VALOUR BLUE",
-    img:
-      "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-  },
-  "jordan-mars-270-london": {
-    name: "JORDAN MARS 270 LONDON",
-    img:
-      "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-  },
-  "air-jordan-1-zoom-racer-blue": {
-    name: "RACER BLUE",
-    img:
-      "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1"
-  }
-};
+    "air-jordan-3-valor-blue": {
+      name: "VALOUR BLUE",
+      img: "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+    },
+    "jordan-mars-270-london": {
+      name: "JORDAN MARS 270 LONDON",
+      img: "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+    },
+    "air-jordan-1-zoom-racer-blue": {
+      name: "RACER BLUE",
+      img: "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+    },
+  };
+
+
 
 export default App;
+
+
+// function App() {
+  //   return (
+  //     <Router>
+  //       <nav>
+  //         <Link to="/">Home</Link>
+  //         <Link to="/launch">Launch</Link>
+  //         <Link to="/launches">LaunchIndex</Link>
+  //       </nav>
+  
+  //       <Routes>  
+  //         <Route path="/" element={<Home />} />
+  //         <Route path="launch" element={<Launch />} />
+  //         <Route path="/" element={<LaunchIndex />} />
+  //         <Route path=":slug" element={<LaunchIndex />} />
+  //         <Route />
+  //         <Route path="*" element={<NotFound />} />
+  //       </Routes>
+        
+  //     </Router>
+  //   );
+  // }
+  
+  // function NotFound() {
+  //   return (
+  //     <div>
+  //       <h1>Sorry your page is not found</h1>
+  //     </div>
+  //   );
+  // }
+  
+  // function Home() {
+  //   return (
+  //     <div>
+  //       <h1>Welcome Home</h1>
+  //     </div>
+  //   );
+  // }
+  
+  // function Launch() {
+  //   return (
+  //     <div>
+  //       <h1>Launch</h1>
+        
+  //       <Outlet />
+  //     </div>
+  //   );
+  // }
+  
+  // function LaunchIndex() {
+  //   return (
+  //     <ul>
+  //       {Object.entries(shoes).map(([slug, { name, img }]) => (
+  //         <li key={slug}>
+  //           <Link to={"/Launch/${slug}"}>
+  //             <h2>{name}</h2>
+  //             <img src={img} alt={name} />
+  //           </Link>
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
+  
+  // function LaunchShoe() {
+  //   return <div>shoe</div>;
+  // }
+  
+  // const shoes = {
+  //   "air-jordan-3-valor-blue": {
+  //     name: "VALOUR BLUE",
+  //     img: "https://secure-images.nike.com/is/image/DotCom/CT8532_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  //   },
+  //   "jordan-mars-270-london": {
+  //     name: "JORDAN MARS 270 LONDON",
+  //     img: "https://secure-images.nike.com/is/image/DotCom/CV3042_001_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  //   },
+  //   "air-jordan-1-zoom-racer-blue": {
+  //     name: "RACER BLUE",
+  //     img: "https://secure-images.nike.com/is/image/DotCom/CK6637_104_A_PREM?$SNKRS_COVER_WD$&align=0,1",
+  //   },
+  // };
+  
